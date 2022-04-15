a2b03e444d808014192c7baf312302739caf1f85
hunimoda <hunimoda@gmail.com>
2022-04-15 16:30:19 +0900

    Do not use sparse array!

--- Chap27_Array/index.js
+++ Chap27_Array/index.js
@@ -30,19 +30,46 @@
 //// 예제 27-09
 //console.log(Object.getOwnPropertyDescriptors([1, 2, 3]));
 
-// 예제 27-11
-const arr = [];
-console.time("Array Performance Test");
+//// 예제 27-11
+//const arr = [];
+//console.time("Array Performance Test");
+//
+//for (let i = 0; i < 10000000; i++) {
+//	arr[i] = i;
+//}
+//console.timeEnd("Array Performance Test");
+//
+//const obj = {};
+//console.time("Object Performance Test");
+//
+//for (let i = 0; i < 10000000; i++) {
+//	obj[i] = i;
+//}
+//console.timeEnd("Object Performance Test");
 
-for (let i = 0; i < 10000000; i++) {
-	arr[i] = i;
-}
-console.timeEnd("Array Performance Test");
+//// 예제 27-13
+//const arr = [1, 2, 3];
+//console.log(arr.length);
+//
+//arr.push(4);
+//console.log(arr.length);
+//
+//arr.pop();
+//console.log(arr.length);
 
-const obj = {};
-console.time("Object Performance Test");
+//// 예제 27-14
+//const arr = [1, 2, 3, 4, 5];
+//arr.length = 3;
+//console.log(arr);
+//
+//const arr2 = [1];
+//arr2.length = 3;
+//console.log(arr2.length);
+//console.log(arr2);
 
-for (let i = 0; i < 10000000; i++) {
-	obj[i] = i;
-}
-console.timeEnd("Object Performance Test");
+//// 예제 27-17
+//const sparse = [, 2, , 4];
+//console.log(sparse.length);
+//console.log(sparse);
+//
+//console.log(Object.getOwnPropertyDescriptors(sparse));
