ef9c10835f73bf3408de2a7499a636eb3a964111
hunimoda <hunimoda@gmail.com>
2022-04-15 16:09:24 +0900

    27.1 What is array?

---
+++ Chap27_Array/index.js
@@ -0,0 +1,12 @@
+//  // 예제 27-04
+//  const arr = ["apple", "banana", "orange"];
+//
+//  console.log(arr.length);
+//  for (let i = 0; i < arr.length; i++) {
+//  	console.log(arr[i]);
+//  }
+
+//  //  예제 27-06
+//  const arr = [1, 2, 3];
+//  console.log(arr.constructor === Array);
+//  console.log(Object.getPrototypeOf(arr) === Array.prototype);
