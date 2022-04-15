654345eabf820368bd61155f186e378e6294fdc2
hunimoda <hunimoda@gmail.com>
2022-04-15 16:21:54 +0900

    Javascript array is NOT an array

--- Chap27_Array/index.js
+++ Chap27_Array/index.js
@@ -10,3 +10,39 @@
 //  const arr = [1, 2, 3];
 //  console.log(arr.constructor === Array);
 //  console.log(Object.getPrototypeOf(arr) === Array.prototype);
+
+//// 예제 27-08
+//function linearSearch(array, target) {
+//	const length = array.length;
+//
+//	for (let i = 0; i < length; i++) {
+//		if (array[i] === target) {
+//			return i;
+//		}
+//	}
+//
+//	return -1;
+//}
+//
+//console.log(linearSearch([1, 2, 3, 4, 5, 6], 3));
+//console.log(linearSearch([1, 2, 3, 4, 5, 6], 0));
+
+//// 예제 27-09
+//console.log(Object.getOwnPropertyDescriptors([1, 2, 3]));
+
+// 예제 27-11
+const arr = [];
+console.time("Array Performance Test");
+
+for (let i = 0; i < 10000000; i++) {
+	arr[i] = i;
+}
+console.timeEnd("Array Performance Test");
+
+const obj = {};
+console.time("Object Performance Test");
+
+for (let i = 0; i < 10000000; i++) {
+	obj[i] = i;
+}
+console.timeEnd("Object Performance Test");
